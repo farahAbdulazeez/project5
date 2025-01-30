efb19153b47fb46446de0c1159ff7030ad4b2506
Farah <Farah@DESKTOP-4IKORCG.lan>
2025-01-29 19:58:36 -0600

    implemented add()

--- src/Demo1/DemoTest5.java
+++ src/Demo1/DemoTest5.java
@@ -2,7 +2,16 @@ package Demo1;
 
 public class DemoTest5 {
 	public static void main(String args []) {
-		System.out.println("Java");
+		System.out.println("Farah");
+		int a = 10;
+		int b = 20;
+		int result = a +b;
+		
+		System.out.println(result);
+	}
+	
+	public void add(int a, int b) {
+		System.out.println(a+b);
 	}
 
 }
